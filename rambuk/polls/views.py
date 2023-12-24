96c120f983b483d62e61045132cd4f9e406f11a3
Rambuk01 <festersenmario@gmail.com>
2023-12-24 14:37:35 +0100

    views added

--- rambuk/polls/views.py
+++ rambuk/polls/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. Youre at the polls index now.")
+
+def detail(request, question_id):
+    return HttpResponse(f'Youre looking at question {question_id}')
+
+def result(request, question_id):
+    return HttpResponse(f'Youre looking at the result of question {question_id}')
+
+def vote(request, question_id):
+    return HttpResponse(f'Youre voting on question {question_id}')

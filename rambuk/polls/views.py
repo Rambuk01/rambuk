a110c9c68b8a95f45c57285e3eb8cd42115567c4
Rambuk01 <festersenmario@gmail.com>
2023-12-25 00:59:22 +0100

    added templates

--- rambuk/polls/views.py
+++ rambuk/polls/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, HttpResponse
-
+from django.template import loader
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. Youre at the polls index now.")
+    lastest_question_list = Question.objects.order_by("-pub_date")[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': lastest_question_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse(f'Youre looking at question {question_id}')

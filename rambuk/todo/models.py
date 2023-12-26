dd9fcacd7f312bef0ed548d0facd6cff3465523b
Rambuk01 <festersenmario@gmail.com>
2023-12-26 21:31:09 +0100

    added todomodel

--- rambuk/todo/models.py
+++ rambuk/todo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    title = models.CharField(max_length=100, null=True)
+    description = models.CharField(max_length=255)
+    completed = models.BooleanField(default = False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
--- rambuk/todo/views.py
+++ rambuk/todo/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
+from .models import TodoItem
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello there!")
+    todos = TodoItem.objects.all()
+    return render(request, 'todo/index.html', {'todos': todos})
+
+    #return HttpResponse("Hello there!", {'request': request})

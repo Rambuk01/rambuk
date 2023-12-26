b69b226a0bd54d957075bd95268d77fb3eedb3e1
Rambuk01 <festersenmario@gmail.com>
2023-12-26 22:33:23 +0100

    form added
    todo form added

--- rambuk/todo/urls.py
+++ rambuk/todo/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'todo'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('create-todo/', views.create_todo, name='create-todo'),
 ]
--- rambuk/todo/views.py
+++ rambuk/todo/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, Http404
 from .models import TodoItem
+from .forms import TodoItemForm
 # Create your views here.
 def index(request):
     todos = TodoItem.objects.all()
     return render(request, 'todo/index.html', {'todos': todos})
 
     #return HttpResponse("Hello there!", {'request': request})
+
+def create_todo(request):
+    
+    if request.method == 'POST':
+        form = TodoItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            todos = TodoItem.objects.all()
+            return render(request, 'todo/index.html', {'todos': todos})
+    else:
+        form = TodoItemForm()
+    return render(request, 'todo/create-todo.html', {'form': form})

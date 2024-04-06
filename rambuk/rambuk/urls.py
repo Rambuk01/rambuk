8245408e17c105e27813728d93848ff72157383a
Rambuk01 <festersenmario@gmail.com>
2024-04-06 20:46:22 +0200

    minor

--- rambuk/rambuk/urls.py
+++ rambuk/rambuk/urls.py
@@ -17,9 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+#from django.urls import re_path
+#from django.views.static import serve
+#from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
     path('todo/', include('todo.urls')),
-    path('', include('vizualytics.urls'))
+    path('', include('vizualytics.urls')),
+    #re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    #re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
+
